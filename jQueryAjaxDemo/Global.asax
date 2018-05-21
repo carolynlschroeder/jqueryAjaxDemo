0c67fd6a424f6b285a94f79c89c795346f1119d2
Carolyn <carolynlschroeder@hotmail.com>
2018-05-21 06:05:49 -0500

    Add project files.

---
+++ jQueryAjaxDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jQueryAjaxDemo.MvcApplication" Language="C#" %>
---
+++ jQueryAjaxDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQueryAjaxDemo.Startup))]
+namespace jQueryAjaxDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
